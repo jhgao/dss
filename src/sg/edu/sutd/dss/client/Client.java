26110895bc37dbf99741e6aa094dbd8dfe62a616
jhgao <gaojianhan@gmail.com>
2012-10-11 15:38:00 +0800

    Modify build.xml. Test build client CLI.

--- src/sg/edu/sutd/dss/client/Client.java
+++ src/sg/edu/sutd/dss/client/Client.java
@@ -1,8 +1,36 @@
 package sg.edu.sutd.dss.client;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+
 public class Client {
 
-	public static void main(String[] args){
-		
+	public static void main(String[] args) {
+		parseCmd(args);
+	}
+
+	private static void parseCmd(String[] args) {
+		Options opts = new Options();
+		opts.addOption("s", "storefile", true, "store a local file into dss");
+
+		CommandLineParser parser = new PosixParser();
+		CommandLine cmd;
+		try {
+			cmd = parser.parse(opts, args);
+
+			if (cmd.hasOption("s")) {
+				System.out.println("has options store with tgt = " + cmd.getOptionValue("s"));
+			} else {
+				HelpFormatter formatter = new HelpFormatter();
+				formatter.printHelp("client", opts);
+			}
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			System.err.println("Parsing failed.  Reason: " + e.getMessage());
+		}
 	}
 }
